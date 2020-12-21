1b733c459ce7f919d586a63bba7205b3da43bb72
IHPC course <ihpc2061@fs3.cm.cluster>
2020-12-21 19:06:51 +0100

    MPI helloworld

---
+++ Makefile
@@ -0,0 +1,11 @@
+# Variables 
+
+CC = gcc
+CFLAGS = -g -Wall
+
+#Targets
+check-syntax:
+	${CC} -o nul -S ${CHK_SOURCES}
+
+clean:
+	$(RM) helloworld *.o *~ *.dat *.txt
---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "mpi.h"
+
+int rank,np;
+int main(int argc, char **argv)
+{
+  MPI_Init(&argc,&argv);
+  MPI_Comm_size(MPI_COMM_WORLD,&np);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+  printf("Node %i of %i says:Hello World!\n",rank,np);
+
+  MPI_Finalize();
+}
